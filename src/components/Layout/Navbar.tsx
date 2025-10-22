2b74dc386464fc53fb2edb8f07d1229cc175d590
shuvajitpal <62866829+shuvajitpal@users.noreply.github.com>
2025-10-22 21:20:02 +0530

    modify Navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -12,11 +12,11 @@ export default function Navbar() {
   const favourite = <Image src={`${theme === "dark" ? "/heart-l.png" : "/heart-b.png"}`} alt="logo" width={25} height={25} />
   const change = <Image src={`${theme === "dark" ? "/moon.png" : "/sun.png"}`} alt="logo" width={25} height={25} />
 
-  const bg =`${theme === "dark" ?  "hover:bg-gray-400 shadow-[0_0_10px_#f472b6]" : "hover:bg-gray-700 shadow-[0_0_10px_#fef3c7]"}`;
+  const bg =`${theme === "dark" ?  " hover:shadow-[0_0_10px_#f472b6]" : " hover:shadow-[0_0_10px_#fef3c7]"}`;
 
   return (
-    <nav className={`${theme === "dark" ? "bg-white text-black"
-      : "bg-black text-white"} flex justify-between items-center px-6 py-1 transition-all duration-500`}>
+    <nav className={`${theme === "dark" ? "bg-white/90 text-black"
+      : "bg-gray-900/90 text-white"} sticky top-0 z-50 backdrop-blur-sm flex justify-between items-center px-6 py-1 transition-all duration-500`}>
       <div className="flex items-start -ml-4">{rss}
         <h1 className="text-2xl font-extrabold mt-1.5">NewsPortal</h1>
       </div>
