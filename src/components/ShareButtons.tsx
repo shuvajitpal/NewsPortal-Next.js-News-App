03d6f6945cd1ab30753d6136ad8a5e13484be969
shuvajitpal <62866829+shuvajitpal@users.noreply.github.com>
2025-10-23 10:31:01 +0530

    modify Share Button Styles

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -33,37 +33,37 @@ export default function ShareButtons({ url, title }: ShareButtonProps) {
   const cp = <Image src="/cpw.png" alt="" width={14} height={20} />
 
   return (
-    <div className="border-t border-gray-500 pt-4 flex flex-wrap gap-2 ">
+    <div className="border-t border-gray-500 pt-4 flex flex-wrap gap-2">
       <span className={`text-sm font-semibold mt-0.5 ${theme === "dark" ? "text-gray-600" : "text-gray-300"}`}>Share:</span>
       <a
         href={twitterShareUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center px-4 py-0.5 bg-blue-500 text-white rounded-full text-sm hover:bg-blue-600 transition-colors shadow-md gap-1"
+        className="flex items-center px-4 py-0.5 bg-blue-500 text-white rounded-full text-sm hover:bg-blue-600 transition-colors shadow-md gap-1 shadow-white/40"
       ><span>{tw}</span>Twitter
       </a>
       <a
         href={linkedinShareUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center px-4 py-0.5 bg-blue-800 text-white rounded-full text-sm hover:bg-blue-900 transition-colors shadow-md gap-1"
+        className="flex items-center px-4 py-0.5 bg-blue-800 text-white rounded-full text-sm hover:bg-blue-900 transition-colors shadow-md gap-1 shadow-white/40"
       ><span>{lin}</span>Linkedin
       </a>
       <a
         href={whatsappShareUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center px-4 py-0.5 bg-green-500 text-white rounded-full text-sm hover:bg-green-600 transition-colors shadow-md gap-1"
+        className="flex items-center px-4 py-0.5 bg-green-500 text-white rounded-full text-sm hover:bg-green-600 transition-colors shadow-md gap-1 shadow-white/60"
       ><span>{wp}</span>WhatsApp
       </a>
       <a
         href={facebookShareUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center px-4 py-0.5 bg-blue-700 text-white rounded-full text-sm hover:bg-blue-800 transition-colors shadow-md gap-1"
+        className="flex items-center px-4 py-0.5 bg-blue-700 text-white rounded-full text-sm hover:bg-blue-800 transition-colors shadow-md gap-1 shadow-white/40"
       ><span>{fb}</span>Facebook
       </a>
-      <button onClick={handleCopy} className="flex items-center px-4 py-0.5 bg-gray-500 text-white rounded-full text-sm hover:bg-gray-400 transition-colors shadow-md gap-1">
+      <button onClick={handleCopy} className="flex items-center px-4 py-0.5 bg-gray-500 text-white rounded-full text-sm hover:bg-gray-400 transition-colors shadow-md gap-1 shadow-white/20">
        <span>{cp}</span> Copy
       </button >
       {copied && <span className="text-sm text-green-500 mt-0.5">Copied!</span>}
